768b0cf39789e05ea1c6340c38bb3c240506db0f
Анатолий Протасов <mrzip.pro123321@gmail.com>
2026-03-02 21:12:32 +0300

    В класс Book добавлен конструктор класса и метод генерации

--- Bookstore/Book.cs
+++ Bookstore/Book.cs
@@ -5,6 +5,9 @@
     /// </summary>
     public class Book
     {
+        private static int lastId = 0;
+        private static readonly Random rnd = new Random();
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
@@ -21,19 +24,52 @@
         /// <param name="genre">Жанр книги</param>
         /// <param name="pageNumber">Количество страниц</param>
         /// <param name="price">Цена</param>
-        public Book(int id, string name, string author, string genre, int pageNumber, decimal price)
+        public Book(
+            string name = null,
+            string author = null,
+            string genre = null,
+            int? pageNumber = null,
+            decimal? price = null)
         {
-            Id = id;
-            Name = name;
-            Author = author;
-            Genre = genre;
-            PageNumber = pageNumber;
-            Price = price;
+            Id = GetId();
+            Name = name ?? GetRandomLineFromFile("BooksNames.txt"); ;
+            Author = author ?? GetRandomLineFromFile("BooksAuthors.txt"); ;
+            Genre = genre ?? GetRandomLineFromFile("BooksGenres.txt"); ;
+            PageNumber = pageNumber ?? rnd.Next(50, 1001); // 50 - 1000 cтраниц
+            Price = price ?? (decimal)rnd.Next(100, 1501); // 100 - 1500 руб
         }
 
-        public void BookGeniratoin(Book book)
+        private static int GetId()
         {
-        //Генерация
+            lastId++;
+            return lastId;
+        }
+
+        private static string GetRandomLineFromFile(string path)
+        {
+            int count = 0;
+
+            //Первый проход для получения количества строк в файле
+            using (var reader = new StreamReader(path))
+            {
+                while (reader.ReadLine() != null)
+                    count++;
+            }
+
+            if (count == 0)
+                throw new Exception("Empty file");
+
+            int targetIndex = rnd.Next(count); // 0..count-1
+
+            //Второй проход для чтения самой строки
+            using (var reader = new StreamReader(path))
+            {
+                for (int i = 0; i < targetIndex; i++)
+                    reader.ReadLine();
+
+                return reader.ReadLine() ?? string.Empty;
+            }
+
         }
     }
 }
